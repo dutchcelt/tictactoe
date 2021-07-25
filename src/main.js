0cc132bd966ea0b363d89f75f7f2cd108c1d8b75
dutchcelt <studio@dutchcelt.com>
2021-07-25 18:13:46 +0000

    Prettier broke it. fixed conditional checkwinner.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
-import tictactoe from "ticatactoe";
+import tictactoe from 'ticatactoe';
 
 class tictactoeElement extends HTMLElement {
-  constructor() {
-    super();
-    const shadow = this.attachShadow({ mode: "open" });
-    shadow.innerHTML = `<link rel="stylesheet" href="styles/tictactoe.css" media="screen" />`;
-  }
+    constructor() {
+        super();
+        const shadow = this.attachShadow({ mode: 'open' });
+        shadow.innerHTML = `<link rel="stylesheet" href="styles/tictactoe.css" media="screen" />`;
+    }
 
-  connectedCallback() {
-    tictactoe(this.shadowRoot);
-  }
+    connectedCallback() {
+        tictactoe(this.shadowRoot);
+    }
 }
-customElements.define("trvt-tictactoe", tictactoeElement);
+customElements.define('trvt-tictactoe', tictactoeElement);
