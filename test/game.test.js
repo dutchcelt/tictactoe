6bfc6318e80b1768d7148d6cbc1e051e66fdd14d
dutchcelt <studio@dutchcelt.com>
2021-07-28 11:59:09 +0000

    basic test

--- test/game.test.js
+++ test/game.test.js
@@ -3,27 +3,27 @@ import { init } from '../src/scripts/tictactoe.js';
 import { createBoard, placeOnBoard } from '../src/scripts/board.js';
 import browserEnv from 'browser-env';
 
-browserEnv(['window', 'document', 'navigator']);
+browserEnv(['document']);
 
 test('main object test', (t) => {
-    const gameObj = init();
+    const gameObj = init(document.body, 3);
     t.assert(gameObj.turn === 0);
     t.assert(gameObj.SIDE === 3);
-    t.assert(gameObj.winner === false);
     t.assert(gameObj.NUMBEROFSQUARES === 9);
+    t.assert(gameObj.winner === false);
     t.assert(typeof gameObj.handleEvent === 'function');
     t.assert(gameObj.elem.tagName === 'MAIN');
 });
 
 test('create board', (t) => {
-    const gameObj = init();
+    const gameObj = init(document.body, 3);
     createBoard.call(gameObj);
     t.assert(gameObj.elem.className === gameObj.REFERENCE);
     t.assert(gameObj.elem.children.length === gameObj.NUMBEROFSQUARES);
 });
 
 test('placeOnBoard', (t) => {
-    const gameObj = init();
+    const gameObj = init(document.body, 3);
     const mark = 'X';
     createBoard.call(gameObj);
 
