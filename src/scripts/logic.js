69b6465de1a8d5bf323a02526f29a85f1d15f10f
dutchcelt <studio@dutchcelt.com>
2021-07-30 12:53:46 +0000

    Minor cleanup (using bind())

--- src/scripts/logic.js
+++ src/scripts/logic.js
@@ -3,7 +3,11 @@ import { default as findMatchingMarks, other } from './findMatchingMarks.js';
 import { placeOnBoard } from './board.js';
 
 function calculateNextMove(matrix, mark, c) {
-    const [flag, block] = findMatchingMarks.call(this, ...arguments, this.boardSize);
+    const [flag, block] = findMatchingMarks.call(
+        this,
+        ...arguments,
+        this.boardSize
+    );
     return (
         flag === c &&
         block.reduce((w, v) => (matrix[v.x][v.y] === '' ? v : w), false)
@@ -26,24 +30,23 @@ export function nextMove() {
     let even = [0, 2, 6, 8];
     let odd = [1, 3, 5, 7];
 
+    const thisNextMove = calculateNextMove.bind(this);
+    const thisOther = other.bind(this);
+
     let newPos = -1;
     const mark = this.matrix[this.currentAxes.x][this.currentAxes.y];
-    const winningMove = calculateNextMove.call(
-        this,
-        this.matrix,
-        other.call(this, mark),
-        2
-    );
-    const nextCoord =
-        winningMove || calculateNextMove.call(this, this.matrix, mark, 2); // Blocking move;
+    const winningMove = thisNextMove(this.matrix, thisOther(mark), 2);
+    const nextCoord = winningMove || thisNextMove(this.matrix, mark, 2); // Blocking move;
 
-    if (this.boardStateArray.reduce((c, v, i) => (v === mark ? c + i : c)) > 10) {
+    if (
+        this.boardStateArray.reduce((c, v, i) => (v === mark ? c + i : c)) > 10
+    ) {
         even.reverse();
         odd.reverse();
     }
     if (nextCoord) {
         newPos = this.boardSize * nextCoord.x + nextCoord.y;
-    } else if (center === '') {
+    } else if (!center) {
         newPos = 4;
     } else {
         [...even, ...odd].some((i) => {
