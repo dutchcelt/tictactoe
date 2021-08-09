feb6db9f7e0513b243194b8d707c44e0c1bf42fc
dutchcelt <studio@dutchcelt.com>
2021-08-09 16:26:54 +0000

    removed redundant function

--- src/scripts/moves.spec.js
+++ src/scripts/moves.spec.js
@@ -1,12 +1,10 @@
 import test from 'ava';
 import getEveryWinningMove from './moves.js';
 
-const arrayOf = (size) => getEveryWinningMove(size);
-
 test('check the move patterns', (t) => {
     // Number of patterns are two diagonals and rows and columns
     // A board of 3 by 3 would have 8 patterns
-    t.true(arrayOf(3).length === 8);
+    t.true(getEveryWinningMove(3).length === 8);
     // A board of 5 by 5 would have 12 patterns
-    t.true(arrayOf(5).length === 12);
+    t.true(getEveryWinningMove(5).length === 12);
 });
