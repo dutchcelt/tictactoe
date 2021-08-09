a01e6f954e4318a6e1752865fcad6e2150118cd8
dutchcelt <studio@dutchcelt.com>
2021-08-09 09:50:25 +0000

    Allow (re)setting of game object

--- src/scripts/tictactoe.js
+++ src/scripts/tictactoe.js
@@ -2,29 +2,21 @@ import { createBoard, placeOnBoard } from './board.js';
 import opts from '../vars/const.js';
 import handleEvent from './events.js';
 
-export const gameObject = Object.create({
-    ...opts,
-    boardSize: undefined,
-    NUMBEROFSQUARES: undefined,
-    parent: undefined,
-    elem: undefined,
-    turn: 0,
-    boardStateArray: undefined,
-    matrix: [
-        ['', '', ''],
-        ['', '', ''],
-        ['', '', ''],
-    ],
-    winner: false,
-    lock: false,
-    handleEvent: handleEvent,
-});
+function init(parent, size) {
+    this.boardSize = size;
+    this.NUMBEROFSQUARES = Math.pow(size, 2);
+    this.parent = parent;
+    this.elem = document.createElement('main');
+    this.turn = 0;
+    this.boardStateArray = new Array(Math.pow(size, 2)).fill('');
+    this.winner = false;
+    this.lock = false;
+    this.handleEvent = handleEvent;
+}
+
+export const gameObject = Object.create(opts);
 
 export default function tictactoe(parent = document.body, size = 3) {
-    gameObject.elem = document.createElement('main');
-    gameObject.parent = parent;
-    gameObject.boardSize = size;
-    gameObject.NUMBEROFSQUARES = Math.pow(size, 2);
-    gameObject.boardStateArray = new Array(Math.pow(size, 2)).fill('');
+    init.call(gameObject, parent, size);
     createBoard();
 }
