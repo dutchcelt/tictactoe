eb52d9c6b40fccfabe2d5123509d30d81848dfdc
dutchcelt <studio@dutchcelt.com>
2024-01-13 12:42:19 +0100

    Dont use the main tag in a custom element

--- src/scripts/tictactoe.js
+++ src/scripts/tictactoe.js
@@ -4,20 +4,20 @@ import handleEvent from './events.js';
 import getEveryWinningMove from './moves.js';
 
 export const init = (parent, size) => ({
-    boardSize: size,
-    parent: parent,
-    elem: document.createElement('main'),
-    turn: 0,
-    boardStateArray: new Array(Math.pow(size, 2)).fill(''),
-    winner: false,
-    lock: false,
-    handleEvent: handleEvent,
-    winningMoves: getEveryWinningMove(size),
+	boardSize: size,
+	parent: parent,
+	elem: document.createElement('div'),
+	turn: 0,
+	boardStateArray: new Array(Math.pow(size, 2)).fill(''),
+	winner: false,
+	lock: false,
+	handleEvent: handleEvent,
+	winningMoves: getEveryWinningMove(size),
 });
 
 export const gameObject = Object.create(settings);
 
 export default function tictactoe(parent = document.body, size = 3) {
-    Object.assign(gameObject, init(parent, size));
-    createBoard();
+	Object.assign(gameObject, init(parent, size));
+	createBoard();
 }
